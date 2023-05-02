6d1b4ca942c43803e5b6ae40e40a3c6bec049ad8
Seclud <82933148+Seclud@users.noreply.github.com>
2023-05-02 13:05:47 +0500

    Сделал ранний макет для странички с целями

--- BucketListMAUI/TaskPage.xaml.cs
+++ BucketListMAUI/TaskPage.xaml.cs
@@ -1,9 +1,41 @@
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
 namespace BucketListMAUI;
 
 public partial class TaskPage : ContentPage
 {
-	public TaskPage()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<Task> Notes { get; set; }
+    public TaskPage(TaskViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+        Notes = new ObservableCollection<Task>();
+
+        var notesJson = Preferences.Get("Notes", "");
+        if (!string.IsNullOrEmpty(notesJson))
+        {
+            var savedNotes = JsonConvert.DeserializeObject<List<Task>>(notesJson);
+            Notes.Clear();
+            foreach (var note in savedNotes)
+            {
+                Notes.Add(note);
+            }
+        }
+    }
+   /* private async void AddNoteButton_Clicked(object sender, EventArgs e)
+    {
+        // Show the AddNotePage as a modal popup
+        var addNotePage = new NewTaskPage();
+        await Navigation.PushModalAsync(addNotePage);
+    }*/
+    /* private void NoteListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+     {
+         // Show the selected note's text in the editor
+         var selectedNote = e.SelectedItem as Task;
+         if (selectedNote != null)
+         {
+             TextEditor.Text = selectedNote.Text;
+         }
+     }*/
 }
---
+++ BucketListMAUI/TaskViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BucketListMAUI
+{
+    public partial class TaskViewModel: ObservableObject
+    {
+        public TaskViewModel() 
+        {
+            Items = new ObservableCollection<string>();        
+        }
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]
+        void Delete(string s)
+        {
+            if (Items.Contains(s))
+                Items.Remove(s);
+        }
+    }
+}
